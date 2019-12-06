4094cf6f2b25e1255a4f1936f79f5b81626e3fcf
Nicola <39983382+nisol91@users.noreply.github.com>
2019-12-06 18:07:41 +0100

    edit corporate model

--- lib/models/corporate_model.dart
+++ lib/models/corporate_model.dart
@@ -2,35 +2,44 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Corporate {
   String id;
-  String description;
   String name;
+  String description;
+  String address;
   String img;
-  Timestamp creationDate;
+  String corporateType;
   bool featured;
+  Timestamp creationDate;
 
-  Corporate(
-      {this.id,
-      this.description,
-      this.name,
-      this.img,
-      this.creationDate,
-      this.featured});
+  Corporate({
+    this.id,
+    this.name,
+    this.description,
+    this.address,
+    this.img,
+    this.corporateType,
+    this.featured,
+    this.creationDate,
+  });
 
   Corporate.fromMap(Map snapshot, String id)
       : id = id ?? '',
-        description = snapshot['description'] ?? '',
         name = snapshot['name'] ?? '',
+        description = snapshot['description'] ?? '',
+        address = snapshot['address'] ?? '',
         img = snapshot['img'] ?? '',
-        creationDate = snapshot['creationDate'],
-        featured = snapshot['featured'] ?? false;
+        corporateType = snapshot['corporateType'] ?? '',
+        featured = snapshot['featured'],
+        creationDate = snapshot['creationDate'];
 
   toJson() {
     return {
-      "description": description,
       "name": name,
+      "description": description,
+      "address": address,
       "img": img,
-      "creationDate": creationDate,
+      "corporateType": corporateType,
       "featured": featured,
+      "creationDate": creationDate,
     };
   }
 }
